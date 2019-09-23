4ef1ab82773661dd380fdcd1ef33a86bc7d5590d
Ovidiu Turcoane <ovidiuturcoane@yahoo.com>
2019-09-23 22:09:58 +0300

    App3.Recommit.

--- src/App-4.test.js
+++ src/App-4.test.js
@@ -3,27 +3,29 @@ import React from 'react';
 import {shallow} from 'enzyme';
 
 describe('App', () => {
-    let wrapper;
-    beforeEach(() => {
-        wrapper = shallow(<App/>);
-    });
     it('should have the `th` "Items"', () => {
+        const wrapper = shallow(
+            <App/>
+        );
         expect(
             wrapper.contains(<th>Items</th>)
         ).toBe(true);
     });
     it('should have a `button`', () => {
+        const wrapper = shallow(<App/>);
         expect(wrapper.containsMatchingElement(
             <button>Add item</button>
         )).toBe(true);
     });
     it('`button` should be disabled', () => {
+        const wrapper = shallow(<App/>);
         const button = wrapper.find('button').first();
         expect(
             button.props().disabled
         ).toBe(true);
     });
     it('should have an `input` element', () => {
+        const wrapper = shallow(<App/>);
         expect(
             wrapper.containsMatchingElement(<input/>)
         ).toBe(true);
